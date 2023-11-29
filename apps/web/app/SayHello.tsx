af3f996f105c6d8a5a6d0d3ed9929f003a52dfcd
Piotr Kulpinski <piotr@kulpinski.pl>
2023-11-29 09:25:47 +0100

    Update react types

--- apps/web/app/SayHello.tsx
+++ apps/web/app/SayHello.tsx
@@ -3,10 +3,14 @@
 import { api } from "../utils/trpc"
 
 export const SayHello = () => {
-  const { data, isLoading } = api.example.sayHello.useQuery({ name: 'Piotr' })
+  const { data, isLoading, isSuccess } = api.example.sayHello.useQuery({ name: 'Piotr' })
 
   if (isLoading) {
     return <div>Loading...</div>
+  }
+
+  if (!isSuccess) {
+    return null
   }
 
   return (
