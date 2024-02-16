a58d632070ed007d51df5081ba46a6b23f2bb9dd
Piotr Kulpinski <piotr@kulpinski.pl>
2024-02-16 21:31:54 +0100

    update form color picker

--- apps/web/components/form/controls/FormColorPicker.tsx
+++ apps/web/components/form/controls/FormColorPicker.tsx
@@ -1,95 +1,35 @@
-import { Popover } from "@curiousleaf/design"
-import { isLightColor } from "@curiousleaf/utils"
-import type { PopoverContent } from "@radix-ui/react-popover"
-import Sketch from "@uiw/react-color-sketch"
-import { cva } from "class-variance-authority"
-import { XIcon } from "lucide-react"
-import type { ComponentPropsWithoutRef, MouseEventHandler } from "react"
+import { ColorPicker, ColorPickerElement, ColorPickerProps } from "@curiousleaf/design"
+import { forwardRef } from "react"
 import { useFormContext } from "react-hook-form"
-
-import { IconCheckerboard } from "~/components/interface/icons/checkerboard"
 import { config } from "~/config"
+
 import { useFieldContext } from "~/providers/FieldProvider"
 
-const clearerVariants = cva(
-  [
-    "absolute left-3 top-1/2 grid -translate-y-1/2 place-items-center opacity-0 transition-opacity",
-    "hover:!opacity-100 group-hover/picker:opacity-75",
-  ],
-  {
-    variants: {
-      light: {
-        true: "text-black",
-        false: "text-white",
-      },
-    },
+export type FormColorPickerProps = Omit<ColorPickerProps, "onChange" | "onClear">
+
+export const FormColorPicker = forwardRef<ColorPickerElement, FormColorPickerProps>(
+  (props, ref) => {
+    const { watch } = useFormContext()
+    const { field } = useFieldContext()
+    const color = watch(field.name)
+
+    const onChange = (value: string) => {
+      field.onChange(value)
+    }
+
+    const onClear = () => {
+      field.onChange("")
+    }
+
+    return (
+      <ColorPicker
+        ref={ref}
+        color={color}
+        presetColors={config.presetColors}
+        onChange={onChange}
+        onClear={color && onClear}
+        {...props}
+      />
+    )
   },
 )
-
-export type FormColorPickerProps = Omit<ComponentPropsWithoutRef<typeof PopoverContent>, "content">
-
-export const FormColorPicker = ({ className, ...props }: FormColorPickerProps) => {
-  const { watch } = useFormContext()
-  const { field } = useFieldContext()
-  const color = watch(field.name)
-
-  // const buttonClass = formInputVariants({
-  //   hasError: !!fieldState.error,
-  //   hoverable: true,
-  //   fullWidth: false,
-  //   className,
-  // })
-
-  const handleColorChange = ({ hex }: { hex: string }) => {
-    field.onChange(hex.toLocaleUpperCase())
-  }
-
-  const handleClear: MouseEventHandler = e => {
-    e.preventDefault()
-    field.onChange("")
-  }
-
-  return (
-    <div className="group/picker relative">
-      <Popover
-        popover={
-          <Sketch
-            color={color}
-            onChange={handleColorChange}
-            presetColors={config.presetColors}
-            className="!shadow-none"
-            disableAlpha
-          />
-        }
-        className="!p-0"
-        {...props}
-      >
-        <button className={""}>
-          <div className="relative -ml-1 mr-1 size-5 overflow-hidden rounded border">
-            <IconCheckerboard className="size-full opacity-25" />
-
-            {!!color && <div className="absolute inset-0" style={{ backgroundColor: color }} />}
-          </div>
-
-          <div className="grow truncate">
-            {color ? (
-              <span className="font-mono leading-4">{color}</span>
-            ) : (
-              <span className="opacity-75">Pick color</span>
-            )}
-          </div>
-        </button>
-      </Popover>
-
-      {!!color && (
-        <button
-          type="button"
-          className={clearerVariants({ light: !!color && isLightColor(color) })}
-          onClick={handleClear}
-        >
-          <XIcon className="pointer-events-none size-3.5" />
-        </button>
-      )}
-    </div>
-  )
-}
