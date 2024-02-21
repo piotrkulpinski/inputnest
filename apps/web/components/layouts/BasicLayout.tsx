be76e3a34ceb6d0b97847f1c7d1b337af14d27b9
Piotr Kulpinski <piotr@kulpinski.pl>
2024-02-21 12:50:20 +0100

    ✨ feat: add workspace switcher

--- apps/web/components/layouts/BasicLayout.tsx
+++ apps/web/components/layouts/BasicLayout.tsx
@@ -14,24 +14,26 @@ type BasicLayoutProps = HTMLAttributes<HTMLDivElement> & {
 
 export const BasicLayout = ({ children, title, description }: BasicLayoutProps) => {
   return (
-    <Card className="fixed left-1/2 top-[10vh] max-h-[calc(90vh-2rem)] w-[calc(100vw-2rem)] max-w-sm -translate-x-1/2 overflow-y-auto">
-      <Card.Panel className="flex flex-col items-start gap-3">
-        <Logo asChild>
-          <Link href={config.routes.dashboard}>{config.title}</Link>
-        </Logo>
+    <div className="fixed left-1/2 top-[10dvh] max-h-[calc(90dvh-2rem)] w-[calc(100vw-2rem)] max-w-sm flex flex-col gap-4 -translate-x-1/2">
+      <Card className="overflow-y-auto">
+        <Card.Panel className="flex flex-col items-start gap-3">
+          <Logo asChild>
+            <Link href={config.routes.dashboard}>{config.title}</Link>
+          </Logo>
 
-        <H3 className="mt-5">{title}</H3>
+          <H3 className="mt-5">{title}</H3>
 
-        {!!description && (
-          <Paragraph size="sm" className="opacity-60">
-            {description}
-          </Paragraph>
-        )}
-      </Card.Panel>
+          {!!description && (
+            <Paragraph size="sm" className="opacity-60">
+              {description}
+            </Paragraph>
+          )}
+        </Card.Panel>
 
-      <Card.Panel theme="gray" asChild>
-        {children}
-      </Card.Panel>
-    </Card>
+        <Card.Panel theme="gray" asChild>
+          {children}
+        </Card.Panel>
+      </Card>
+    </div>
   )
 }
--- packages/database/src/schema/workspace.ts
+++ packages/database/src/schema/workspace.ts
@@ -5,18 +5,21 @@ import { Theme } from "@prisma/client"
 import { colorSchema, idSchema, isReservedSlug, slugSchema } from "./index"
 
 export const workspaceSchema = z.object({
-  name: z.string().trim().min(3),
-  slug: slugSchema.min(3).refine(slug => !isReservedSlug(slug), {
-    message: "This slug name is reserved",
-  }),
+  name: z.string().trim().min(3).default(""),
+  slug: slugSchema
+    .min(3)
+    .refine(slug => !isReservedSlug(slug), {
+      message: "This slug name is reserved",
+    })
+    .default(""),
   theme: z.nativeEnum(Theme).default(Theme.System),
   logo: z.string().nullish(),
   brandColor: colorSchema
     .nullish()
-    .default("#0051FF")
     .refine(color => !color || !isLightColor(color), {
       message: "Choose a color that has a better contrast ratio with white",
-    }),
+    })
+    .default("#0051FF"),
 })
 
 export const createWorkspaceSchema = workspaceSchema
