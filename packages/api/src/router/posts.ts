d085b311e48c235460c4f0071e68009658525c64
Piotr Kulpinski <piotr@kulpinski.pl>
2024-02-22 12:15:35 +0100

    🔨 chore: update schemas and queries for posts and comments routers

--- packages/api/src/router/posts.ts
+++ packages/api/src/router/posts.ts
@@ -1,54 +1,47 @@
-import {
-  createPostSchema,
-  idSchema,
-  postRelationSchema,
-  updatePostSchema,
-} from "@inputnest/database"
+import { idSchema, postSchema } from "@inputnest/database"
 
-import { protectedProcedure, router } from "../trpc"
+import { router, workspaceProcedure } from "../trpc"
 
 export const postsRouter = router({
-  getAll: protectedProcedure
-    .input(postRelationSchema)
-    .query(async ({ ctx: { db, userId }, input: { workspaceId } }) => {
-      return await db.post.findMany({
-        where: { workspace: { id: workspaceId, members: { some: { userId } } } },
-        orderBy: { createdAt: "desc" },
-        include: { board: true, status: true, _count: { select: { comments: true, votes: true } } },
-      })
-    }),
+  getAll: workspaceProcedure.query(async ({ ctx: { db }, input: { workspaceId } }) => {
+    return await db.post.findMany({
+      where: { workspaceId },
+      orderBy: { createdAt: "desc" },
+      include: { board: true, status: true, _count: { select: { comments: true, votes: true } } },
+    })
+  }),
 
-  get: protectedProcedure
-    .input(idSchema.merge(postRelationSchema))
-    .query(async ({ ctx: { db, userId }, input: { id, workspaceId } }) => {
+  get: workspaceProcedure
+    .input(idSchema)
+    .query(async ({ ctx: { db }, input: { id, workspaceId } }) => {
       return await db.post.findFirstOrThrow({
-        where: { id, workspace: { id: workspaceId, members: { some: { userId } } } },
+        where: { id, workspaceId },
         include: { author: true, _count: { select: { comments: true, votes: true } } },
       })
     }),
 
-  create: protectedProcedure
-    .input(createPostSchema)
-    .mutation(async ({ ctx: { db, userId }, input }) => {
+  create: workspaceProcedure
+    .input(postSchema)
+    .mutation(async ({ ctx: { db, userId }, input: { ...data } }) => {
       return await db.post.create({
-        data: { ...input, authorId: userId },
+        data: { ...data, authorId: userId },
       })
     }),
 
-  update: protectedProcedure
-    .input(updatePostSchema)
-    .mutation(async ({ ctx: { db }, input: { id, ...data } }) => {
+  update: workspaceProcedure
+    .input(postSchema.merge(idSchema))
+    .mutation(async ({ ctx: { db }, input: { id, workspaceId, ...data } }) => {
       return await db.post.update({
-        where: { id },
+        where: { id, workspaceId },
         data,
       })
     }),
 
-  delete: protectedProcedure
+  delete: workspaceProcedure
     .input(idSchema)
-    .mutation(async ({ ctx: { db, userId }, input: { id } }) => {
+    .mutation(async ({ ctx: { db }, input: { id, workspaceId } }) => {
       return await db.post.delete({
-        where: { id, workspace: { members: { some: { userId } } } },
+        where: { id, workspaceId },
       })
     }),
 })
--- packages/database/src/schema/post.ts
+++ packages/database/src/schema/post.ts
@@ -1,19 +1,10 @@
 import * as z from "zod"
 
-import { idSchema } from "./index"
-
 export const postSchema = z.object({
   title: z.string().trim().min(1, "This field is required"),
   content: z.string().nullish(),
   boardId: z.string().min(1, "This field is required"),
   statusId: z.string().min(1, "This field is required"),
 })
-
-export const postRelationSchema = z.object({
-  workspaceId: z.string(),
-})
-
-export const createPostSchema = postSchema.merge(postRelationSchema)
-export const updatePostSchema = postSchema.merge(idSchema)
 
 export type PostSchema = z.infer<typeof postSchema>
